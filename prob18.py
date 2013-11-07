7acb1e9843391080da4483ed64851c8c4d49b456
Lucy Lin <Lucy@cardinalblue.com>
2013-11-07 15:31:20 +0800

    minor amendments

--- prob18.py
+++ prob18.py
@@ -1,28 +1,28 @@
-from LogisticRegression import *
+from Util import *
 
 trainData, Ntrain = LoadData('hw3_train.dat')
-testData = LoadData('hw3_test.dat')
+testData, Ntest = LoadData('hw3_test.dat')
 # trainData[i][0] : x of data i
 # trainData[i][1] : y of data i
-eta = 0.01
+print('finish loading data!')
+
+eta = 0.001
 T = 2000
 
 #train
-#wf = LogisticRegression(trainData, Ntrain, T, eta)
-#print wf
+wf = LogisticRegression(trainData, Ntrain, T, eta)
+print wf
+print ('finish training')
 
 # test
-#Eout = Test(testData, wf)
-N = len(testData[0])
-#print Eout
-#print N
-#print Eout / 1.0 / N
+Eout = Test(testData, wf)
+print Eout / 1.0 / Ntest
+
 
 #stochastic
-
 #train
 eta = 0.001
 w = StochasticGradient(trainData, Ntrain, T, eta)
 print w
 E_sto = Test(testData, w)
-print E_sto / 1.0 / N
+print E_sto / 1.0 / Ntest
